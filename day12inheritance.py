78a5fb75da1d5cf608becddc373840738084eb5c
anartikna <anartikna@gmail.com>
2020-04-29 10:51:36 -0700

    classes in python3
    classes in python3

---
+++ day12inheritance.py
@@ -0,0 +1,69 @@
+'''
+Objective
+Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video!
+
+Task
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string, .
+A string, .
+An integer, .
+An integer array (or vector) of test scores, .
+A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
+Input Format
+
+The locked stub code in your editor calls your Student class constructor and passes it the necessary arguments. It also calls the calculate method (which takes no arguments).
+
+You are not responsible for reading the following input from stdin:
+The first line contains , , and , respectively. The second line contains the number of test scores. The third line of space-separated integers describes .
+
+Constraints
+
+Output Format
+
+This is handled by the locked stub code in your editor. Your output will be correct if your Student class constructor and calculate() method are properly implemented.
+'''
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        a = sum(self.scores) / len(self.scores)
+        if a < 40:
+            return "T"
+        elif (40 <= a) and (a < 55):
+            return "D"
+        elif (55 <= a) and (a < 70):
+            return "P"
+        elif (70 <= a) and (a < 80):
+            return "A"
+        elif (80 <= a) and (a < 90):
+            return "E"
+        elif (90 <= a) and (a <= 100):
+            return "O"
+        else:
+            return ""
+
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
